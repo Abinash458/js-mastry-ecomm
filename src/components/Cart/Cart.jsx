3084abe217093aa3c3b17cced01bfb52fcde78ab
Abinash Mohapatra <abinash@expensebit.com>
2020-12-08 22:25:10 +0530

    implement react-router

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
@@ -9,7 +10,7 @@ const Cart = ({ cart }) => {
 
     const EmptyCart = () => (
         <Typography variant="subtitle1">
-            You have no items in your shopping cart, start adding some!
+            You have no items in your shopping cart, <Link className={classes.link} to="/">Start adding some!</Link>
         </Typography>
     )
 
